99ba2eb0a42a02a490de96e5ac408d7a1653e07c
Muhammad Abdul Rosip <m.abdrosip@gmail.com>
2023-10-04 12:52:43 +0700

    push

---
+++ lib/presentation/auth/pages.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+import 'package:email_validator/email_validator.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:kumpulan/config/component/ui/common/custom_form_button.dart';
+import 'package:kumpulan/config/component/ui/common/custom_input_field.dart';
+import 'package:kumpulan/config/component/ui/common/page_header.dart';
+import 'package:kumpulan/config/component/ui/common/page_heading.dart';
+
+part 'login.dart';
+part 'register.dart';
+part 'forgot_passoword.dart';
